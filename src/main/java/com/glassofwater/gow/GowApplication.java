2712a91b2e4bcd316e9eeb15504662bfac0e74c5
rrango <nsbazhov@edu.hse.ru>
2022-04-06 18:09:12 +0300

    Initial commit

---
+++ src/main/java/com/glassofwater/gow/GowApplication.java
@@ -0,0 +1,13 @@
+package com.glassofwater.gow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GowApplication.class, args);
+    }
+
+}
